707fff899d4aaded85c16d0db57263ce8ecdbccc
Lion-R <i@lion-r.com>
2018-10-09 16:14:40 +0800

    Update plyr.js
    更新svg，删除冗余代码

--- src/plyr.js
+++ src/plyr.js
@@ -9,23 +9,4 @@
       c.innerHTML=a.responseText;
       b.insertBefore(c,b.childNodes[0])
   }
-})(document,"https://cdn.bootcss.com/plyr/3.3.9/plyr.svg");
-plyr.setup({
-    controls: ["play", "current-time", "duration", "mute", "volume", "captions", "fullscreen"],
-    tooltips:{ controls: true, seek: false },
-    i18n: {
-    restart:            "重播",
-    rewind:             "快退{seektime} 秒",
-    play:               "播放",
-    pause:              "暂停",
-    forward:            "快进{seektime}秒",
-    buffered:           "缓冲",
-    currentTime:        "当前",
-    duration:           "时长",
-    volume:             "音量",
-    toggleMute:         "静音",
-    toggleCaptions:     "字幕",
-    toggleFullscreen:   "全屏",
-},
-    showPosterOnEnd:true,
-});
+})(document,"https://cdn.plyr.io/3.3.9/plyr.svg");
