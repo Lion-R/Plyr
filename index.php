32e5b38e6bbb7e965a2d048313d5afb87e8ba1e4
LCG <i@lion-r.com>
2021-10-27 20:31:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ License: A "Slug" license name e.g. GPL2
 
 // 调用插件文件
 function themtuts_plyr_css_and_js_files() {
-    echo "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/plyr@3.6.8/dist/plyr.css?ver=3.6.8'>";
-    echo "<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/plyr@3.6.8/dist/plyr.js?ver=3.6.8'></script>";
+    echo "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/plyr@3.6.9/dist/plyr.css?ver=3.6.9'>";
+    echo "<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/plyr@3.6.9/dist/plyr.js?ver=3.6.9'></script>";
     echo "<script> const player = new Plyr('#player');</script> ";
 }
 
